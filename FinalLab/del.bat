3106622731d19bcda8a65198d1382495dca15053
Diogo Miguel Simões <46751892+diogomgsimoes@users.noreply.github.com>
2020-03-06 23:45:13 +0000

    Add files via upload

---
+++ FinalLab/del.bat
@@ -0,0 +1,10 @@
+del *.obj /s /q
+del *.idb /s /q
+del *.pdb /s /q
+rem del c:\str2017\*.exe /s /q
+del *.ilk /s /q
+del *.obj /s /q
+del *.pch /s /q
+del *.db /s /q
+del *.sdf /s /q
+del *.ipch /s /q
